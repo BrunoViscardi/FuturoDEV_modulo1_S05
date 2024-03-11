31f43695bbcf5b8e5c6d3d5615d3b12517a4fd0f
Bruno Viscardi <bruno.viscardi33@gmail.com>
2024-03-11 20:22:20 -0300

    otimização da função consulta

--- script.js
+++ script.js
@@ -1,38 +1,56 @@
 
 
 
-document.getElementById("botao-consulta").addEventListener('click', consultar);
+document.getElementById("botao-consulta").addEventListener('click', consultarApresentar);
+
+
+
+function consultarApresentar() {
+
+    escolha = consultar();
+
+    if (typeof escolha === 'number') {
+        document.getElementById("valor-produto").innerHTML = "RS " + escolha
+
+    } else {
+        document.getElementById("valor-produto").innerHTML = escolha
+    }
+}
+
 
 
 function consultar() {
+
+    let produtoDesejado = document.getElementById("Produto").value.toUpperCase();
+
     let escolha = undefined;
-    switch (document.getElementById("Produto").value.toUpperCase()) {
+    switch (produtoDesejado) {
         case "MORANGO":
         case "01":
-            escolha = "R$ 11,99"
+            escolha = 11.99
             break;
         case "02":
         case "MAÇA":
-            escolha = "R$5,89"
+            escolha = 5.89
             break;
         case "BANANA":
         case "03":
-            escolha = "R$3,74"
+            escolha = 3.74
             break;
         case "MELANCIA":
         case "04":
-            escolha = "R$20,51"
+            escolha = 20.51
             break;
         case "05":
         case "ABACATE":
-            escolha = "R$7,50"
+            escolha = 7.50
             break;
         default:
             escolha = "Não há correspondência"
             break;
     }
 
-    document.getElementById("valor-produto").innerHTML = escolha
+    return escolha;
 
 }
 
